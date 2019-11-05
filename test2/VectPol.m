cb14116029f43852cbe81d5b686b0a8289baaa90
3800660 <solene.saulnier@etu.upmc.fr>
2019-11-05 12:51:37 +0100

    version anterieur sans test 2 car il est sur la branche

--- test2/VectPol.m
+++
@@ -1,8 +0,0 @@
-function [res]=VectPol(m,sigma_m,taille,theta_min,theta_max)
-res=zeros(2,taille);
-
-for i=1:taille
-    res(1,i)=normrnd(m,sigma_m);
-    res(2,i)=unifrnd(theta_min,theta_max);
-end
-end
